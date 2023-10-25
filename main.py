e2084e1976a499b72f2f250a8619681775c0a352
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-25 21:14:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Crear una calculadora con las operaciones básicas.
+valor1 = 23
+valor2 = 30
+
+resultadoSuma = valor1 + valor2
+resultadoResta = valor1 - valor2
+resultadoMultiplicacion = valor1 * valor2
+resultadoDivision =  valor1 / valor2
+
+print("El resultado de la suma es:", resultadoSuma ,"\n",
+      "el resultado de la resta es:", resultadoResta,"\n",
+      "el resultado de la multiplicacion es:", resultadoMultiplicacion, "\n",
+      "el resultado de la division es:", resultadoDivision)
